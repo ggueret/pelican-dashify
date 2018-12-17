bbe1601690f7ca91393bb87347aa5de2ed7871b2
Geoffrey GUERET <g.gueret@gmail.com>
2018-12-17 04:58:36 +0100

    include package data

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     license="MIT",
 
     packages=["pelican_dashify"],
+    include_package_data=True,
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     install_requires=["pelican>=3.7"],
     classifiers=[
