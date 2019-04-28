327e48239ddb18c8946103852f0700c9827bc523
Geoffrey GUERET <g.gueret@gmail.com>
2019-04-28 23:02:06 +0200

    dropped Python 3.4 support, it's deprecated and too costly to integrate

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
 
     packages=["pelican_dashify"],
     include_package_data=True,
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     install_requires=["pelican>=3.7"],
     test_requires=["pytest>=4.4"],
     classifiers=[
@@ -30,7 +30,6 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
