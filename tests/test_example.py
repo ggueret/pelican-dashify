f2847fca67530505d1c400ad61377d13b5a9ffb8
Geoffrey GUERET <g.gueret@gmail.com>
2019-04-28 22:11:42 +0200

    mp4box change generated paths names on newer versions

--- tests/test_example.py
+++ tests/test_example.py
@@ -9,29 +9,19 @@ EXAMPLE_DIRECTORY = os.path.join(PARENT_DIRECTORY, "example")
 def test_project_example(tmp_path_factory):
     from pelican import Pelican, read_settings
 
-    OUTPUT_PATH = str(tmp_path_factory.getbasetemp())
+    OUTPUT_PATH = tmp_path_factory.getbasetemp()
 
     settings = read_settings(
         os.path.join(EXAMPLE_DIRECTORY, "pelicanconf.py"),
         override={
             "PATH": os.path.join(EXAMPLE_DIRECTORY, "content"),
-            "OUTPUT_PATH": OUTPUT_PATH
+            "OUTPUT_PATH": str(OUTPUT_PATH)
         })
 
     pelican = Pelican(settings)
     pelican.run()
 
-    EXPECTED_FILES = [
-        "videos/trailer_1080p.mov/.cache/1920x1080_4976k.mp4",
-        "videos/trailer_1080p.mov/.cache/960x540_1244k.mp4",
-        "videos/trailer_1080p.mov/.cache/480x270_311k.mp4",
-        "videos/trailer_1080p.mov/.cache/audio_128k.mp4",
-        "videos/trailer_1080p.mov/1920x1080_4976k_dash_track1_init.mp4",
-        "videos/trailer_1080p.mov/960x540_1244k_dash_track1_init.mp4",
-        "videos/trailer_1080p.mov/480x270_311k_dash_track1_init.mp4",
-        "videos/trailer_1080p.mov/audio_128k_dash_track1_init.mp4",
-        "videos/trailer_1080p.mov/manifest.mpd"
-    ]
-
-    for file_path in EXPECTED_FILES:
-        assert os.path.isfile(os.path.join(OUTPUT_PATH, file_path))
+    assert len(list(OUTPUT_PATH.glob("videos/trailer_1080p.mov/.cache/*.mp4"))) == 4  # noqa: E501
+    assert len(list(OUTPUT_PATH.glob("videos/trailer_1080p.mov/*.mp4"))) == 4
+    assert OUTPUT_PATH.joinpath("videos/trailer_1080p.mov/manifest.mpd").is_file()
+    # todo: read the manifest to validate related files
