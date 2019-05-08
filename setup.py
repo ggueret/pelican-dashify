8d12ea6690a596cf8d380d11dd18b71f2948c16d
Geoffrey Guéret <geoffrey.gueret@ubicast.eu>
2019-05-08 16:46:52 +0200

    fixed setup error related to file encoding

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
 
     description="Pelican-dashify let you to convert proper MPEG-DASH content "
                 "generated from your videos with Pelican.",
-    long_description=open(os.path.join(BASEDIR_PATH, "README.md"), "r").read(),
+    long_description=open(os.path.join(
+        BASEDIR_PATH, "README.md"), "r").decode("utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/ggueret/pelican-dashify",
     license="MIT",
