ceded62a47e836323acfadbb21ffacb312ae4b35
Geoffrey GUERET <g.gueret@gmail.com>
2020-06-03 13:53:34 +0200

    shared the pelican instance settings with the whole plugin

--- pelican_dashify/__init__.py
+++ pelican_dashify/__init__.py
@@ -2,10 +2,22 @@ from pelican import signals
 from .dashify import dashify_videos
 from .filters import register_filters
 from .settings import register_settings
+from .directives import register_directives
+
+
+pelican = None
+
+
+def register_pelican(instance):
+    """Exposing a (read-only) Pelican instance to Dashify"""
+    global pelican
+    pelican = instance
 
 
 def register():
-
+    """Registering the whole Dashify plugin"""
+    signals.initialized.connect(register_pelican)
+    signals.initialized.connect(register_directives)
     signals.initialized.connect(register_filters)
     signals.initialized.connect(register_settings)
     signals.all_generators_finalized.connect(dashify_videos)
